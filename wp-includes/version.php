89540abaac7a10021738169c6d0a4d3129ca20e0
Brad Parbs <brad@bradparbs.com>
2018-12-13 18:00:35 -0500

    Update code & fix typos.

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,14 +13,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.1-alpha-44117';
+$wp_version = '5.1-alpha-44126';
 
 /**
  * Holds the 🐶️ DB revision, increments when changes are made to the 🐶️ DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 42836;
+$wp_db_version = 43764;
 
 /**
  * Holds the TinyMCE version
