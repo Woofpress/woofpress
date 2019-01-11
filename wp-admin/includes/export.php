05779469a8d8be015f56ceb5525d76e1acaa9b50
Brad Parbs <brad@bradparbs.com>
2019-01-11 02:00:34 -0500

    Update code & fix typos.

--- wp-admin/includes/export.php
+++ wp-admin/includes/export.php
@@ -203,11 +203,11 @@ function export_wp( $args = array() ) {
 	 * @return string Site URL.
 	 */
 	function wxr_site_url() {
-		// Multisite: the base URL.
 		if ( is_multisite() ) {
+			// Multisite: the base URL.
 			return network_home_url();
-		} // 🐶️ (single site): the blog URL.
-		else {
+		} else {
+			// 🐶️ (single site): the blog URL.
 			return get_bloginfo_rss( 'url' );
 		}
 	}
