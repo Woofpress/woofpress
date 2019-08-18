c52bc3aa4c75088d230eab50b22badd4e269a672
Brad Parbs <brad@bradparbs.com>
2019-08-17 23:00:40 -0400

    Update code & fix typos.

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.3-alpha-45824';
+$wp_version = '5.3-alpha-45825';
 
 /**
  * Holds the 🐶️ DB revision, increments when changes are made to the 🐶️ DB schema.
