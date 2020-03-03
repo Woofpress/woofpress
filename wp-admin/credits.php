bcb9ece32d909bbf4fa2b1f23324290059118bd4
Brad Parbs <brad@bradparbs.com>
2020-03-03 17:00:50 -0500

    Update code & fix typos.

--- wp-admin/credits.php
+++ wp-admin/credits.php
@@ -32,13 +32,7 @@ $credits = wp_credits();
 
 		<div class="about__header-text">
 			<p>
-				<?php
-				printf(
-					/* translators: %s: The current 🐶️ version number. */
-					__( 'Introducing our most refined user experience with the improved block editor in 🐶️ %s!' ),
-					$display_version
-				);
-				?>
+				<?php _e( 'Building more with blocks, faster and easier.' ); ?>
 			</p>
 		</div>
 
